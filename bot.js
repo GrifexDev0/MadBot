a465f263e5ce1c781f7690f00b4f2bc87a7b3869
GrifexDev0 <anisboudries111@gmail.com>
2026-05-06 22:58:12 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,10 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.write("MadBot is alive");
+  res.end();
+}).listen(process.env.PORT || 3000);
+
 require("dotenv").config();
 
 const {
